b6cb711f1929bd6a8c8d924bca9d18371cc782be
Sirlan_br <158532654+Sirlanns@users.noreply.github.com>
2024-09-11 17:12:25 -0300

    Add files via upload

---
+++ 04-aula.py
@@ -0,0 +1,24 @@
+# operadores aritmeticos:
+valorA = float(input("Digite o valor de A: "))
+valorB = float(input("Digite o valor de B: "))
+# Soma:
+soma = valorA + valorB
+print(f"A soma = ",soma)
+# Subtração:
+subtração = valorA - valorB
+print(f"A subtração = ",subtração)
+# Multiplicão:
+multiplicação = valorA * valorB
+print(f"A multiplicação = ",multiplicação)
+# Divisão:
+divisão = valorA / valorB
+print(f"A divisão = ",divisão)
+# Divisão inteira:
+divisão_inteira = valorA // valorB
+print(f"A divisão inteira = ",divisão_inteira)
+# Resto da divisão:
+resto_divisão = valorA % valorB
+print(f"resto da divisão =",resto_divisão)
+# Exponenciação:
+exponenciação = valorA ** valorB
+print(f"exponenciação =",exponenciação)
