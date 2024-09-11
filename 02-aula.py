2c7dd6a53ad67d399febdfe3091ad054b2ed6e8f
Sirlan_br <158532654+Sirlanns@users.noreply.github.com>
2024-09-11 14:56:17 -0300

    Add files via upload

---
+++ 02-aula.py
@@ -0,0 +1,12 @@
+print("informe o valor de x:")
+x = float(input())
+print("informe o valor de y:")
+y = float(input())
+soma = x + y
+subtração = x - y
+multiplicação = x * y
+divisão = x / y
+print("o resultado da soma é:", soma)
+print("o resultado da subtração é:", subtração)
+print("o resultado da multiplicação é:", multiplicação)
+print("o resultado da divisão é:", divisão)
