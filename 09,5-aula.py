96ce2d5ce68854071b27bba44a210e9f03fade00
Sirlan_br <158532654+Sirlanns@users.noreply.github.com>
2024-10-02 17:34:24 -0300

    Add files via upload

---
+++ 09,5-aula.py
@@ -0,0 +1,4 @@
+def par(x):
+    return x % 2 == 0
+
+print(par(2))
---
+++ 09-aula.py
@@ -0,0 +1,7 @@
+# Declaração da função exibirmensagem(nome)
+def exibirmensagem(nome):
+    print(f"Olá, {nome}!")
+
+# início do meu algoritmo
+nome = input("Digite o seu nome!")
+exibirmensagem(nome) # Exibe a mensagem com nome do usuário.
