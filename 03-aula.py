d0f62815521c9a983da6e7bfa47b4cebd713b980
Sirlan_br <158532654+Sirlanns@users.noreply.github.com>
2024-11-07 17:46:36 -0300

    Update 03-aula.py

---
+++ 03-aula.py
@@ -0,0 +1,17 @@
+# Tipos de Dados Primitivos:
+# - Inteiro (int): números inteiros
+# - Float: números reais, decimais
+# - String(str): cadeia de caracteres, utilizando aspas
+# - Boolean: valores lógicos (True ou False)
+# - Complex (complex): números complexos, com parte real e imaginária
+# - List (list): Lista de elementos, ordenados e indexados
+# - Tuple (tuple): tupla de elementos, ordenados e imutáveis
+# - Dictionary (dict): dicionário de elementos, não ordenados e indexados
+
+# Atribuição de variável por captura
+nome = input('Digite seu nome: ')
+print('Olá', nome)
+idade = input('Digite a sua idade: ')
+print(idade)
+altura = input('Digite a sua altura: ')
+print(altura)
