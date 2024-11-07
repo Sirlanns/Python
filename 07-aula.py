d507b7d77abf9bfc6e6c5405b0ce5686ad5d8dee
Sirlan_br <158532654+Sirlanns@users.noreply.github.com>
2024-11-07 17:46:00 -0300

    Update 07-aula.py

--- 07-aula.py
+++ 07-aula.py
@@ -1,1 +1,21 @@
+#Esse código tem como função indicar o seu índice de massa corporal
 
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a sua altura: "))
+imc = peso/(altura * altura)
+
+if imc < 18.5:
+    print(f"seu imc é {imc:.2f}")
+    print("Você está abaixo do peso!!!")
+
+elif imc >= 18.5 and imc <= 24.9:
+    print(f"seu imc é {imc:.2f}")
+    print("Seu peso está normal")
+
+elif imc >= 25 and imc <= 34.9:
+    print(f"seu imc é {imc:.2f}")
+    print("Você está com sobrepeso")
+
+elif imc >= 35:
+    print(f"seu imc é {imc:.2f}")
+    print("Atenção!!! Você está obeso")
